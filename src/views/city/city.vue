875dc962a26450cf03bee9e56acb5956d7a39aaf
duff <wu-wenhan@outlook.com>
2023-04-16 19:36:48 +0800

    cityGroup 动态请求数据，切换管理

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="city top-page">
-    <!-- 1. 搜索框 -->
-    <form action="/">
-      <van-search 
-        v-model="searchValue" 
-        placeholder="请输入搜索关键词" 
-        show-action 
-        clearable
-        shape="round"
-        @cancel="cancelClick" 
-      />
-    </form>
+    <div class="top">
+      <!-- 1. 搜索框 -->
+      <form action="/">
+        <van-search v-model="searchValue" placeholder="请输入搜索关键词" show-action clearable shape="round"
+          @cancel="cancelClick" />
+      </form>
 
-    <!-- 2. tab 的切换 -->
-    <van-tabs v-model:active="tabActive" color="var(--primary-color)">
-      <!-- <van-tab :title="allCity?.cityGroup?.title"></van-tab> -->
-      <template v-for="(value, key, index) in allCities" :key="key">
-        <van-tab :title="value.title"></van-tab>
+      <!-- 2. tab 的切换 -->
+      <van-tabs v-model:active="tabActive" color="var(--primary-color)" line-height="3">
+        <!-- <van-tab :title="allCity?.cityGroup?.title"></van-tab> -->
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+    </div>
+
+    <div class="content">
+      <!-- 表达式（过长） -->
+      <!-- <template v-for="item in allCities[tabActive]?.cities"> -->
+      <template v-for="item in currentGroup?.cities">
+        <div>{{ item }}</div>
       </template>
-    </van-tabs>
+    </div>
   </div>
 </template>
 
@@ -28,6 +32,7 @@ import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
 // import { getCityAll } from '@/services'
 import useCityStore from '@/stores/moudles/city'
+import { computed } from '@vue/reactivity';
 
 const router = useRouter()
 
@@ -52,11 +57,23 @@ const tabActive = ref()
 //   allCity.value = res.data
 // })
 
+// 从 store 中获取数据
 const cityStore = useCityStore()
 cityStore.fetchAllCitiesData()
 const { allCities } = storeToRefs(cityStore)
 
+// 目的: 获取选中标签后的数据
+// 1.获取正确的 key: 将 tabs 中绑定的 tabAction 正确绑定
+// 2.根据 key 从 allCities 获取数据, 默认直接获取的数据不是响应式的, 所以必须包裹 computed
+const currentGroup = computed(() => allCities.value[tabActive.value])
+
 </script>
 
 <style lang="less" scoped>
+  .city {
+    .content {
+      height: calc(100vh - 98px);
+      overflow-y: auto;
+    }
+  }
 </style>
